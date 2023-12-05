9f5686d10b8daf2b68452cf0acf482eb45301362
lorenzo <lorenzo.orsingher@gmail.com>
2023-12-05 11:21:28 +0100

    add sbs frames

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import cv2 as cv
+import numpy as np
+
 video_path = "data/VID_20231205_104636.mp4"
 cap = cv.VideoCapture(video_path)
 
 
 cv.namedWindow("frame", cv.WINDOW_NORMAL)
 
-while True:
-    ret, frame = cap.read()
+ret, frame1 = cap.read()
+ret, frame2 = cap.read()
 
-    cv.imshow("frame", frame)
-    cv.waitKey(0) #ciaone
+cv.imshow("frame", np.hstack([frame1,frame2]))
+
+cv.waitKey(0)
