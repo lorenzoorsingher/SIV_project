286ae9887777273369e89a7eca6a5325c6d14690
lorenzo <lorenzo.orsingher@gmail.com>
2023-12-20 14:54:51 +0100

    test calib file kitti

--- kitti_test.py
+++ kitti_test.py
@@ -1,18 +1,22 @@
 import os
 import pdb
+import numpy as np
 import cv2 as cv
 from kitti_loader import kittiLoader
 
 
 do_images = "data/data_odometry_gray/dataset/sequences"
 do_poses = "data/data_odometry_poses/dataset/poses"
+do_calib = "data/data_odometry_poses/dataset/sequences"
 
-kl = kittiLoader(do_images, do_poses, 0)
+kl = kittiLoader(do_images, do_poses, do_calib, 0)
 
 cv.namedWindow("frame", cv.WINDOW_NORMAL)
 
-while True:
-    img, pose = kl.next_frame()
 
-    cv.imshow("frame", img)
-    cv.waitKey(1)
+breakpoint()
+# while True:
+#     img, pose = kl.next_frame()
+
+#     cv.imshow("frame", img)
+#     cv.waitKey(1)
