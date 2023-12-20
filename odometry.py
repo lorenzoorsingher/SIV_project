3ba9322246f27da0a47836863b426dc5a6546c86
lorenzo <lorenzo.orsingher@gmail.com>
2023-12-20 15:14:10 +0100

    use last good estimation in case of bad data

--- odometry.py
+++ odometry.py
@@ -16,17 +16,16 @@ class Position:
         self.cumul_R = np.eye(3, 3, dtype=np.float64)
         self.cumul_eulerR = np.array([0, 0, 0], dtype=np.float64)
         self.cumul_t = np.array([0, 0, 0], dtype=np.float64)
+        self.lastgoodpose = np.eye(4, 4)
 
     def update_pos(self, R, t):
-        # self.cumul_t += t
-
-        # self.cumul_R = np.matmul(self.cumul_R, R)
-        # self.cumul_eulerR += self.rotationMatrixToEulerAngles(R)
-        # eulered = self.rotationMatrixToEulerAngles(self.cumul_R)
-
         tmpT = np.eye(4, 4)
         tmpT[:3, :3] = R
         tmpT[:3, 3] = t
+
+        curr_coo = np.array([self.x, self.y, self.z, 1])
+        self.x, self.y, self.z, _ = np.dot(tmpT, curr_coo)
+        self.world_coo = np.dot(tmpT, np.array([0, 0, 0, 1]))
 
         eulered = self.rotationMatrixToEulerAngles(R) * 180 / np.pi
 
@@ -35,25 +34,19 @@ class Position:
             sizestr += "#"
 
         if abs(eulered[1]) >= 10:
-            R = np.eye(3, 3)
-            t *= 0
+            pose = self.lastgoodpose
+            R = self.lastgoodpose[:3, :3]
+            t = self.lastgoodpose[:3, 3]
             sizestr2 = ""
             for i in range(len(sizestr)):
                 sizestr2 += "X"
             sizestr = sizestr2
+        else:
+            self.lastgoodpose = np.eye(4, 4)
+            self.lastgoodpose[:3, :3] = R
+            self.lastgoodpose[:3, 3] = t
 
         print(sizestr)
-        curr_coo = np.array([self.x, self.y, self.z, 1])
-        self.x, self.y, self.z, _ = np.dot(tmpT, curr_coo)
-
-        # print("----------------------------------")
-        # print(abs(abs(R) - np.eye(3, 3)).round(2).mean())
-        # print((self.cumul_eulerR * (180 / np.pi)).round(2))
-        # print(abs(R - np.eye(3, 3)).round(2).mean())
-        # self.y += np.cos(self.cumul_eulerR[1]) * t[0]
-        # self.x += np.sin(self.cumul_eulerR[1]) * t[2]
-
-        # Initial camera pose
 
         # Apply transformations
         self.cumul_R = np.dot(R, self.cumul_R)
@@ -63,10 +56,6 @@ class Position:
 
         self.world_pose[:3, :3] = self.cumul_R.T
         self.world_pose[:3, 3] = np.dot(-(self.cumul_R).T, self.cumul_t)
-
-        self.world_coo = np.dot(tmpT, np.array([0, 0, 0, 1]))
-        # print(self.world_coo.round(2))
-        # print(curr_coo.round(2))
 
     def rotationMatrixToEulerAngles(self, R):
         sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
