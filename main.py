ac3d0cb306975110a3d4744e0ca76398397c6989
lorenzo <lorenzo.orsingher@gmail.com>
2024-01-09 17:14:57 +0100

    fix kitti using wrong calibration file

--- main.py
+++ main.py
@@ -30,32 +30,32 @@ if MODE == "imgs":
     im_paths = [imgs_path + "/" + name for name in os.listdir(imgs_path)]
     im_paths.sort()
     idx = 0
+
+    data = json.load(open(calib_path))
+    mtx = np.array(data[0])
+    dist = np.array(data[1])
 if MODE == "video":
     calib_path = "camera_data/calib.json"
     video_path = "data/room_tour.MOV"
     cap = cv.VideoCapture(video_path)
+
+    data = json.load(open(calib_path))
+    mtx = np.array(data[0])
+    dist = np.array(data[1])
 if MODE == "kitti":
     do_images = "data/data_odometry_gray/dataset/sequences"
     do_poses = "data/data_odometry_poses/dataset/poses"
     do_calib = "data/data_odometry_poses/dataset/sequences"
 
-    kl = kittiLoader(do_images, do_poses, do_calib, 4)
+    kl = kittiLoader(do_images, do_poses, do_calib, 0)
     mtx, dist = kl.get_params()
     maxdist = int(kl.get_maxdist())
 
-data = json.load(open(calib_path))
-mtx = np.array(data[0])
-dist = np.array(data[1])
-proj = np.hstack([mtx, np.array([[0], [0], [0]])])
 
 maxdist = int(maxdist * 1.5)
+
 gt_map = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
-track_map = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
 track_map2 = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
-
-
-updated_gt_map = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
-updated_gt_map = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
 updated_gt_map = np.zeros((maxdist * 2 + 10, maxdist * 2 + 10, 3))
 
 odo = Odometry(mtx, dist, 2)
@@ -80,7 +80,7 @@ def update_map(pose, map):
     newt = np.eye(4, 4)
     newt[:3, :3] = R
     newt[2, 3] = 1
-    nx, y, nz, _ = newt @ forw
+    nx, _, nz, _ = newt @ forw
 
     updated_map = cv.line(
         updated_map,
@@ -92,7 +92,7 @@ def update_map(pose, map):
         (255, 0, 255),
         2,
     )
-    # breakpoint()
+
     eulered = odo.position.rotationMatrixToEulerAngles(R) * 180 / np.pi
     cv2.putText(
         updated_map,
@@ -101,7 +101,7 @@ def update_map(pose, map):
         cv.FONT_HERSHEY_PLAIN,
         updated_map.shape[0] // 100,
         (255, 255, 255),
-        10,
+        updated_map.shape[0] // 100,
         cv2.LINE_AA,
     )
     return updated_map
@@ -144,7 +144,6 @@ while True:
     # )
 
     updated_track_map2 = update_map(odo.position.world_pose[:-1], track_map2)
-
     # tmpPose = np.zeros((3, 4))
     # tmpPose[:3, :3] = odo.position.cumul_R
     # tmpPose[:3, 3] = odo.position.cumul_t
