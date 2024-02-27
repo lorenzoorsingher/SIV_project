2b698e6db04ffd8284d3aa4154953022e23274b5
ilaria <ilariarocchi2@gmail.com>
2024-02-27 23:20:56 +0100

    rotation and aggregation added in evaluation

--- evaluation.py
+++ evaluation.py
@@ -8,10 +8,10 @@ def compute_relative_pose_error(estimated_poses, ground_truth_poses):
 
     errors = []
     for est_pose, gt_pose in zip(estimated_poses, ground_truth_poses):
-        # Align estimated pose with ground truth pose
+        # Aligns estimated pose with ground truth pose
         aligned_est_pose = horn_alignment(est_pose, gt_pose)
 
-        # Compute error between aligned poses
+        # Computes error between aligned poses
         translation_error = compute_translation_error(
             aligned_est_pose.translation, gt_pose.translation
         )
@@ -19,11 +19,11 @@ def compute_relative_pose_error(estimated_poses, ground_truth_poses):
             aligned_est_pose.rotation, gt_pose.rotation
         )
 
-        # Aggregate error
+        # Aggregates error
         total_error = translation_error + rotation_error
         errors.append(total_error)
 
-    # Compute aggregate error metric (RMSE??)
+    # Computes aggregate error metric (RMSE??)
     aggregate_error = compute_aggregate_error(errors)
 
     return aggregate_error
@@ -31,38 +31,40 @@ def compute_relative_pose_error(estimated_poses, ground_truth_poses):
 
 # Alignment of poses with Horn's method
 def horn_alignment(estimated_poses, ground_truth_poses):
-    # Compute centroids (centers of mass of both sets of poses)
+    # Computes centroids (centers of mass of both sets of poses)
     est_centroid = np.mean(estimated_poses, axis=0)
     gt_centroid = np.mean(ground_truth_poses, axis=0)
 
-    # Center the poses (Both sets of poses are translated so that their centroids coincide)
+    # Centers the poses (Both sets of poses are translated so that their centroids coincide)
     centered_est_poses = estimated_poses - est_centroid
     centered_gt_poses = ground_truth_poses - gt_centroid
 
-    # Compute covariance matrix
+    # Computes covariance matrix
     covariance_matrix = np.dot(centered_est_poses.T, centered_gt_poses)
 
-    # Perform SVD (Singular Value Decomposition (SVD) is performed on the covariance matrix to obtain the optimal rotation matrix)
+    # Performs SVD (Singular Value Decomposition (SVD) is performed on the covariance matrix to obtain the optimal rotation matrix)
     U, _, Vt = np.linalg.svd(covariance_matrix)
 
-    # Compute optimal rotation (we extract the optimal rotation matrix from the matrices obtained through SVD)
+    # Computes optimal rotation (we extract the optimal rotation matrix from the matrices obtained through SVD)
     R = np.dot(Vt.T, U.T)
 
-    # Apply rotation (to the aligned poses)
+    # Applies rotation (to the aligned poses)
     aligned_est_poses = np.dot(centered_est_poses, R.T)
 
-    # Compute translation offset (between the aligned poses)
+    # Computes translation offset (between the aligned poses)
     t = gt_centroid - np.dot(R, est_centroid)
 
-    # Apply translation (to align the centroids of the aligned poses)
+    # Applies translation (to align the centroids of the aligned poses)
     aligned_est_poses += t
 
     return aligned_est_poses
 
 
-def compute_translation_error(estimated_translation, ground_truth_translation):
+def compute_translation_error(
+    estimated_translation, ground_truth_translation
+):  # TODO: ADD TO IN PARAMETERS TO compute_relative_pose_error
     """
-    Compute the Euclidean distance between estimated and ground truth translation vectors.
+    Computes the Euclidean distance between estimated and ground truth translation vectors.
 
     Parameters:
         estimated_translation (numpy array): Estimated translation vector.
@@ -74,3 +76,51 @@ def compute_translation_error(estimated_translation, ground_truth_translation):
     # Compute Euclidean distance between translations (estimated and ground truth)
     error = np.linalg.norm(estimated_translation - ground_truth_translation)
     return error
+
+
+def compute_rotation_error(estimated_rotation, ground_truth_rotation):
+    """
+    Computes the rotation error between estimated and ground truth rotation matrices (angle difference between rotations)
+
+    Parameters:
+        estimated_rotation (numpy array): Estimated rotation matrix (3x3).
+        ground_truth_rotation (numpy array): Ground truth rotation matrix (3x3).
+
+    Returns:
+        error (float): Angle difference between estimated and ground truth rotations (in radians).
+    """
+    # Computes the rotation matrix representing the rotation from estimated to ground truth
+    delta_rotation = np.matmul(estimated_rotation.T, ground_truth_rotation)
+
+    # The rotation angle is extracted from the rotation matrix
+    # Axis-angle representation: the angle is the magnitude of the rotation
+    # and the axis is the unit eigenvector (remains in the same direction after a linear transformation is applied to it) corresponding to the eigenvalue (scaling factor) 1 of the rotation matrix
+    # The rotation matrices are assumed to be orthonormal (i.e., proper rotations)
+    cos_theta = (np.trace(delta_rotation) - 1) / 2
+    cos_theta = min(
+        1, max(cos_theta, -1)
+    )  # Ensure cos_theta is within [-1, 1] to avoid numerical errors
+    angle = np.arccos(cos_theta)
+
+    # angle_degrees = np.degrees(angle)
+
+    return angle
+
+
+def compute_aggregate_error(errors):
+    """
+    Computes the aggregate error metric (in this case RMSE) based on a list of individual errors.
+
+    Parameters:
+        errors (list): List of individual error values.
+
+    Returns:
+        aggregate_error (float): Aggregate error metric.
+    """
+    # Convert errors to numpy array for easy computation
+    errors_array = np.array(errors)
+
+    # Compute aggregate error metric (Root Mean Square Error - RMSE)
+    aggregate_error = np.sqrt(np.mean(errors_array**2))
+
+    return aggregate_error
