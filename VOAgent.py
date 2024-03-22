20fbbd4d2c549635de5e555411bee99f8d462221
ilaria <ilariarocchi2@gmail.com>
2024-03-22 16:36:55 +0100

    added little test in VOAgent (for SIFT num_feat)

--- VOAgent.py
+++ VOAgent.py
@@ -123,6 +123,8 @@ class VOAgent:
             self.prevFrame,
             uimg2,
         )
+        # if len(self.prevDes) > 5000:
+        #     print(len(self.prevDes))
 
         # extract rotation and translation
         R, t, bad_data = self.epipolar_computation(pFrame1, pFrame2)
